8e8e5df96b0e2fad404959d8883f3600a1c89169
angc <angcoder.c@gmail.com>
2025-02-01 15:10:38 -0600

    init code

---
+++ src/test/java/com/api/Tests.java
@@ -0,0 +1,17 @@
+package com.api;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Tests {
+    @Test
+    public void test() {
+        assertEquals(true, true);
+    }
+    /*
+     @Test
+     public void test() {
+        // Assert methods
+     }
+     */
+}
