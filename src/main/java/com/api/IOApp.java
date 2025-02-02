313efec25b13107fbc00bb0b222d9d4074cb50b9
angc <angcoder.c@gmail.com>
2025-02-02 17:18:35 -0600

    IO class

---
+++ src/main/java/com/api/IOApp.java
@@ -0,0 +1,110 @@
+package com.api;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+/**
+ * Task:
+ *  Getting arguments from user
+ *      + getParam
+ *  Creating lists of random numbers
+ *      + randomNumbers
+ *          - parseNumber
+ *  Reading and writing files
+ *      + writeNewRandomList
+ *      + readRandomList
+ */
+public class IOApp {
+    
+    public static String getParam(String[] args, String flag) {
+        // string whith user argument
+        // return string
+        String value = null;
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals(flag) && i + 1 < args.length) {
+                value = args[i + 1];
+                break;
+            }
+        }
+        return value;
+    }
+
+    private static Number parseNumber (String item) {
+        Number number=null;
+        try {
+            number = item.contains(".") 
+            ? Double.parseDouble(item) 
+            : Integer.parseInt(item);
+        } catch (Exception e) {}
+        return number;
+    } 
+
+    public static ArrayList<Number> writeNewRandomList (String masterDirPath, String filename, ArrayList<Number> numbers) {
+        
+        // create output file in masterDirPath directory
+        File directory = new File(masterDirPath);
+        File outputFile = new File(directory, filename);
+
+        // write file
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile))) {
+            numbers.stream()
+            .map(Number::toString)
+            .forEach(str -> { 
+                try {
+                    bw.append(str).append("\n");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        } catch (Exception e) {}
+        return numbers;
+    } 
+
+    public static ArrayList<Number> readRandomList (String path) {
+        // read random numbers
+        ArrayList<Number> content = new ArrayList<>();
+        
+        // read lines file
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+
+                // read 1,2,3 format list
+                if (line.contains(",")) {
+
+                    Arrays.stream(line.split("\\s*,\\s*"))
+                    .map(IOApp::parseNumber)
+                    .collect(Collectors.toList())
+                    .forEach(content::add);;
+                
+                } else {
+                    content.add(parseNumber(line));
+                }
+            }
+        } catch (Exception e) {}
+
+        // filter null values
+        content.removeIf(item -> item==null);
+        return content;
+    } 
+
+    public static ArrayList<Number> randomNumbers (Optional<Integer> size) {
+        Random rand = new Random();
+        return rand.ints(
+            size.orElse(3000), 
+            0,
+            300
+        )
+        .boxed()
+        .collect(Collectors.toCollection(ArrayList::new));
+    }
+}
--- src/main/java/com/api/Main.java
+++ src/main/java/com/api/Main.java
@@ -1,7 +1,77 @@
 package com.api;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * Params
+ * 
+ * Help:
+ *      -h
+ *      --help
+ * From exist file
+ *      --order-file <file path>
+ *      --whith-algoritm <name>
+ *      --dir <dir path>
+ *      --output-filename <filename>
+ * Generate file and 
+ *      --gen-rand-nums-file
+ *      --dir <dir path>
+ *      --output-filename <filename>
+ */
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.print("hello wolrd");
+        if (args.length == 0 || IOApp.getParam(args, "-h") != null || IOApp.getParam(args, "-´-help") != null) {
+            printHelp();
+            return;
+        }
+
+        String orderFile = IOApp.getParam(args, "--order-file");
+        String algorithm = IOApp.getParam(args, "--whith-algoritm"); 
+        String dir = IOApp.getParam(args, "--dir");
+        String outputFilename = IOApp.getParam(args, "--output-filename");
+        boolean generateRandomFile = IOApp.getParam(args, "--gen-rand-nums-file")!=null;
+
+        if (orderFile != null && algorithm != null && dir != null && outputFilename != null) {
+            
+        } else if (generateRandomFile && dir != null && outputFilename != null) {
+            
+        } else {
+            System.out.println("ERROR: parametros insuficientes. Usa -h o --help para ver la ayuda.");
+        }
+    }
+
+    private static void printHelp () {
+        System.out.println("""
+        
+        Uso: java -cp out com.api.Main [options]
+        
+        Help:
+            
+            -h, --help => muestra un mensaje de ayuda.
+
+        Ordenar desde un archivo existente:
+            
+            --order-file <file path> => la ruta a un archivo de numero para ordenar.
+            --whith-algoritm <name> => nombre del algoritmo con el que se desea procesar.
+            --dir <dir path> => la ruta del directorio en el que se crea el nuevo archivo.
+            --output-filename <filename> => define el nombre del archivo de salida.
+        
+            Ejemplo:
+            
+            java -cp out com.api.Main --order-file ./input.txt --whith-algoritm quicksort --dir ./carpeta --output-filename ordenado.txt   
+
+        Generar archivo con numeros aleatorios
+            
+            --gen-rand-nums-file => flag que crea un nuevo archivo con numero aleatorios.
+            --dir <dir path> => la ruta del directorio en el que se crea el nuevo archivo..
+            --output-filename <filename> => define el nombre del archivo de salida.
+
+            Ejemplo:
+
+            java -cp out com.api.Main --gen-rand-nums-file --dir ./carpeta --output-filename desordenado.txt       
+        """);
     }
 }
