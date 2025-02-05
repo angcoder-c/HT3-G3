5dc45c3c0d55f60f9c3108996070d55fb0464393
angc <angcoder.c@gmail.com>
2025-02-04 18:48:21 -0600

    quicksort test

--- src/test/java/com/api/Tests.java
+++ src/test/java/com/api/Tests.java
@@ -2,11 +2,23 @@ package com.api;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import com.api.sorts.Quicksort;
 
 public class Tests {
     @Test
     public void test() {
         assertEquals(true, true);
+    }
+
+    @Test
+    public void testQuicksort() {
+        ArrayList<Number> input = new ArrayList<>(Arrays.asList(5, 3, 8, 1, 2));
+        ArrayList<Number> expected = new ArrayList<>(Arrays.asList(1, 2, 3, 5, 8));
+
+        assertEquals(expected, Quicksort.sort(input));
     }
     /*
      @Test
